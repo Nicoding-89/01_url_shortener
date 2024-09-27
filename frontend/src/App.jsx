d6cfffe3db0cc212c3e9456ef2ff172dfcbbcd5c
nicoding89 <ps.nicolas.labarca@gmail.com>
2024-09-26 21:15:23 -0300

    Add Footer component and its style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Urlshortener from './components/UrlShortener'
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Urlshortener />
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+const Footer = () => {
+  return (
+    <section className='footer'>
+      <p className='footer_text'>
+        Copyright © 2024 niCoding <i className="fa-solid fa-laptop-code"></i>
+      </p>
+    </section>
+  )
+}
+
+export default Footer
